50d43b94c2bd7432a368f6c5ffee0cef0cb19a9e
kozluck <kozluck@icloud.com>
2022-03-08 09:12:24 +0100

    Items Visibility changes

--- app/src/main/java/com/example/weatherapp/ViewHandler.java
+++ app/src/main/java/com/example/weatherapp/ViewHandler.java
@@ -1,6 +1,7 @@
 package com.example.weatherapp;
 
 import android.app.Activity;
+import android.view.View;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -24,13 +25,17 @@ public class ViewHandler {
     public void setCityName(String city){
         TextView cityName = (TextView) activity.findViewById(R.id.cityTV);
         cityName.setText(city);
+        cityName.setVisibility(View.VISIBLE);
     }
 
     public void setValuesFromJsonObject(JSONObject jsonObject){
         TextView temperature = (TextView) activity.findViewById(R.id.temperature);
         TextView wind = (TextView) activity.findViewById(R.id.wind);
         TextView pressure = (TextView) activity.findViewById(R.id.pressure);
+        TextView humidity = (TextView) activity.findViewById(R.id.humidity);
         TextView visibility = (TextView) activity.findViewById(R.id.visibility);
+        TextView secondTitle = (TextView) activity.findViewById(R.id.days4);
+
 
         TextView[] nextDays = {
                 (TextView) activity.findViewById(R.id.FirstDayData),
@@ -49,13 +54,21 @@ public class ViewHandler {
                 if(i==0){
                     JSONObject current = jsonObject.getJSONObject("current");
                     temperature.setText(Math.round(current.getDouble("temp") - 273.15) + "℃");
+                    temperature.setVisibility(View.VISIBLE);
                     wind.setText("Wind:"+current.getDouble("wind_speed") + " km/h");
+                    wind.setVisibility(View.VISIBLE);
                     pressure.setText("Pressure:"+current.getInt("pressure") + " hPa");
+                    pressure.setVisibility(View.VISIBLE);
+                    humidity.setText("Humidity:" + current.getInt("humidity") + "%");
+                    humidity.setVisibility(View.VISIBLE);
                     visibility.setText("Visibility:"+current.getInt("visibility")/1000 + "km");
+                    visibility.setVisibility(View.VISIBLE);
+                    secondTitle.setVisibility(View.VISIBLE);
                 }else{
                     JSONObject insideObject = array.getJSONObject(i);
                     JSONObject temp = insideObject.getJSONObject("temp");
                     date = new Date((long)insideObject.getInt("dt")*1000);
+                    nextDays[i-1].setVisibility(View.VISIBLE);
                     nextDays[i-1].setText(df.format(date) + " " + (Math.round(temp.getDouble("day") - 273.15)) + "℃" + "/" + (Math.round(temp.getDouble("night") - 273.15)) + "℃");
                 }
             }
